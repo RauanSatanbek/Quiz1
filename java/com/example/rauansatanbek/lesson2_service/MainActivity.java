b958af8aea383616eb5f11e287166491f67628ae
Rauan Satanbek <rauan.satanbek@gmail.com>
2017-02-11 12:56:34 +0600

    Ds

--- java/com/example/rauansatanbek/lesson2_service/MainActivity.java
+++ java/com/example/rauansatanbek/lesson2_service/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.rauansatanbek.lesson2_service;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.support.v4.app.DialogFragment;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
@@ -48,37 +52,30 @@ public class MainActivity extends AppCompatActivity implements fragment.ChangeTe
     @Override
     public void destroy() {
         Log.d("MyLogs", "destroy");
-        onDestroy();
+        DialogFragment myAlert = new MyAlert(this);
+        myAlert.show(getSupportFragmentManager(), "Dsa");
     }
+    public static class MyAlert extends DialogFragment implements AlertDialog.OnClickListener{
+        MainActivity maimActivity;
+        MyAlert (MainActivity maimActivity) {
+            this.maimActivity = maimActivity;
+        }
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage("Are you sure ?");
+            builder.setNegativeButton("NO", this);
+            builder.setPositiveButton("YES", this);
+            return builder.create();
+        }
 
-    //    public static class MyAlert extends DialogFragment implements AlertDialog.OnClickListener{
-//        int which;
-//        String Text;
-//        MyAlert (int which, String Text) {
-//            this.which = which;
-//            this.Text = Text;
-//        }
-//        @NonNull
-//        @Override
-//        public Dialog onCreateDialog(Bundle savedInstanceState) {
-//            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-//
-//            builder.setMessage("Are you sure ?");
-//            builder.setNegativeButton("NO", this);
-//            builder.setPositiveButton("YES", this);
-//            return builder.create();
-//        }
-//
-//        @Override
-//        public void onClick(DialogInterface dialog, int button) {
-//            switch (which) {
-//                case 1:
-//                    android.support.v4.app.Fragment frag1 = getFragmentManager().findFragmentById(R.id.container);
-//                    ((TextView) frag1.getView().findViewById(R.id.textFrom1))
-//                            .setText(Text);
-//                    break;
-//            }
-//            Log.d("MYLogs", "Which: " + which);
-//        }
-//    }
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            Log.d("MyLogs", "which: " + which);
+            if(which == -1) {
+                maimActivity.onDestroy();
+            }
+        }
+    }
 }
